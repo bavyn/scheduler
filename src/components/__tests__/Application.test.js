f6aba0116b203c317db7c06223a5d3c7fcd2773a
Karma Mann <129702902+bavyn@users.noreply.github.com>
2023-06-25 00:50:23 -0700

    update asyc/await Application test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,27 @@
 import React from "react";
-import {render} from "@testing-library/react"
+import {render, fireEvent, waitForElement} from "@testing-library/react"
 
 import Application from "components/Application";
 
-
 describe("Rendering Application", () => {
-  it("renders without crashing", () => {
-    render(<Application />);
+
+  // it("defaults to Monday and changes the schedule when a new day is selected", () => {
+  //   const { getByText } = render(<Application />);
+  
+  //   return waitForElement(() => getByText("Monday")).then(() => {
+  //     fireEvent.click(getByText("Tuesday"));
+  //     expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  //   });
+  // });
+
+  it("changes the schedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
+  
+    await waitForElement(() => getByText("Monday"));
+  
+    fireEvent.click(getByText("Tuesday"));
+  
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
+
 });
